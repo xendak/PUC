b551f1d4d49d93897021406a72a0235ea3796abe
Rafael Grossi <rg.grossi@outlook.com>
2025-03-12 01:48:05 -0300

    verde

--- AEDS2/verde/anagrams/AnagramChecker.java
+++ AEDS2/verde/anagrams/AnagramChecker.java
@@ -3,27 +3,26 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class AnagramChecker {
-    public static boolean checkAnagrams(String str1, String str2) {
-        str1 = str1.replaceAll("\\s", "").toLowerCase();
-        str2 = str2.replaceAll("\\s", "").toLowerCase();
-        String bStr, sStr;
-
-        if (str1.length() >= str2.length()) {
-            bStr = str1;
-            sStr = str2;
-        } else {
-            bStr = str2;
-            sStr = str1;
-
-        }
+    public static boolean checkAnagrams(StringBuilder bStr, StringBuilder sStr) {
+        boolean result = false;
         for (int i = 0; i < bStr.length(); i++) {
             //sStr = sStr.replaceFirst(Pattern.quote("" + bStr.charAt(i)), "");
             // this can be done without the pattern, but then we lose the control
             // over regex MATCHES like * or .
-            sStr = sStr.replaceFirst("" + bStr.charAt(i), "");
+            boolean replaced = false;
+            for (int j = 0; j < sStr.length(); j++) {
+                Character c = sStr.charAt(j); // im not remaking this function, its basically
+                Character d = bStr.charAt(i); // just Char - 'A' - 'Z'
+                if (Character.toLowerCase(c) == Character.toLowerCase(d)) {
+                    sStr.deleteCharAt(j);
+                    break;
+                }
+            }
+            //sStr = sStr.replaceFirst("" + bStr.charAt(i), "");
         }
         
-        return sStr.equals("");
+        //System.out.println("str1: " + bStr + "\tstr2: " + sStr);
+        return sStr.length() == 0;
     }
 
     public static void main(String[] args) {
@@ -31,11 +30,24 @@ public class AnagramChecker {
 
         String line;
         while (!(line = scanner.nextLine()).equals("FIM")) {
-            String[] words = line.split("-");
-            String str1 = words[0].trim();  // Trim spaces around the words
-            String str2 = words[1].trim();
+            StringBuilder str1 = new StringBuilder();
+            StringBuilder str2 = new StringBuilder();
+            boolean seen = false;
+            for (int i = 0; i <= line.length() - 1; i++) {
+                Character c = line.charAt(i);
+                if (c == ' ') continue;
+                else if (c == '-') {
+                    seen = true;
+                }
+                else if (seen) {
+                    str2.append(c);
+                } else {
+                    str1.append(c);
+                }
+            }
+            //System.out.println("str1: " + str1 + "\tstr2: " + str2);
+            boolean result = str1.length() == str2.length() ? checkAnagrams(str1, str2) : false;
 
-            boolean result = checkAnagrams(str1, str2);
             System.out.println(result ? "SIM" : "NÃO");
         }
 
