71d81f966deef577dc5d17fb6372ec342cc8027f
Rafael Grossi <rg.grossi@outlook.com>
2025-03-26 18:40:32 +0000

    fixed my code for q1, selection sort. && classes

---
+++ AEDS2/provas/1/old.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class medal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[][] medals = new int[n][3];
+        String[] country = new String[n];
+        input.skip("\n");
+
+        for (int i = 0; i < n; i++) {
+            String aux = input.nextLine();
+            //System.out.println(aux);
+            if (aux.length() > 0) {
+                country[i] = parseLine(aux);
+                int help = aux.indexOf(' ');
+                medals[i] = getMedals(aux, n, help+1);
+            }
+        }
+        //System.out.println("Done main, country list");
+        //for (String i : country) {
+        //    System.out.println(i);
+        //}
+        sortOrder(medals, country, n);
+
+        input.close();
+    }
+    
+    public static void swapString(String[] c, int src, int dst) {
+        String aux = c[dst];
+        c[dst] = c[src];
+        c[src] = aux;   
+    }
+    
+    public static void swapInt(int[][] c, int src, int dst) {
+        // gold
+        int aux = c[dst][0];
+        c[dst][0] = c[src][0];
+        c[src][0] = aux;
+        
+        // silver
+        aux = c[dst][1];
+        c[dst][1] = c[src][1];
+        c[src][1] = aux;
+
+        // copper
+        aux = c[dst][2];
+        c[dst][2] = c[src][2];
+        c[src][2] = aux; 
+    }  
+            
+
+    public static void sortOrder(int[][] medals, String[] country, int n) {
+        // gold
+        for (int i = 0; i < n; i++) {
+            int goldi = medals[i][0]; // ouro
+            int silveri = medals[i][1]; // prata
+            int copperi = medals[i][2]; // bronze
+            String countryi = country[i]; // pais
+            // move left
+            for (int j = 0; j < n; j++) {
+                int goldj = medals[j][0]; // ouro
+                int silverj = medals[j][1]; // prata
+                int copperj = medals[j][2]; // bronze
+                String countryj = country[j]; // pais
+
+                boolean toSwap = false;
+
+                if (goldi != goldj) { // j -> i
+                    toSwap = goldj > goldi;
+                } else if (silveri != silverj) {
+                    toSwap = silverj > silveri;
+                } else if (copperi == copperj) {
+                    toSwap = copperj > copperi;
+                } else if (copperi == copperj) {
+                    toSwap = countryj.compareTo(countryi) < 0;
+                }
+
+                if (toSwap) {
+                    swapString(country, j, i);
+                    swapInt(medals, j, i);
+                }
+            }
+        }
+
+        for (int c = n-1; c >= 0; c--) {
+            System.out.printf("%s %d %d %d\n", country[c], medals[c][0], medals[c][1], medals[c][2]);
+        }
+    }
+
+
+    public static int toNumber(String num) {
+        int decimalCase;
+        if (num.length() == 1)
+            decimalCase = 1;
+        else
+            decimalCase = 10 * (num.length() - 1);
+        //System.out.println("o: " + num.length() + "\tn: " + decimalCase);
+        int res = -1;
+        for (int i = 0; i < num.length(); i++) {
+            res = (int) (num.charAt(i) - '0') * (decimalCase);
+            decimalCase = decimalCase / 10;
+        }
+        //System.out.println("f: " + res);
+        return res;
+    }
+
+    public static int[] getMedals(String line, int n, int start) {
+            int[] res = {-1, -1, -1};
+            StringBuilder aux = new StringBuilder();
+            int spaceCount = 0;
+            int medals = 0;
+            //System.out.println(line + ": " + start + ": " + line.charAt(8));
+            for (int i = 0; i < line.length(); i++) {
+                Character c = line.charAt(i);
+                if (c == ' ' && spaceCount == 0) {
+                    spaceCount++;
+                } else if (c == ' ' && spaceCount > 0) {
+                    // TODO: resolve finished string
+                    res[medals] = Integer.parseInt(aux.toString());   // toNumber(aux.toString());
+                    medals++;
+                    aux.setLength(0);
+                } else if (spaceCount > 0 && c != ' ')
+                    aux.append(c);
+            }
+            res[medals] = Integer.parseInt(aux.toString()); //toNumber(aux.toString());
+            return res;
+    }
+
+    public static String parseLine(String line) {
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        Character c = line.charAt(i);
+        while (c != ' ') {
+            res.append(c);
+            i++;
+            c = line.charAt(i);
+        }
+        return res.toString();
+    }
+}
+
--- AEDS2/provas/1/q1.java
+++ AEDS2/provas/1/q1.java
@@ -1,143 +1,136 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class medal {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class q1 {
+  static class Country {
+    private String name;
+    private int gold;
+    private int silver;
+    private int copper;
 
-        int n = input.nextInt();
-        int[][] medals = new int[n][3];
-        String[] country = new String[n];
-        input.skip("\n");
-
-        for (int i = 0; i < n; i++) {
-            String aux = input.nextLine();
-            //System.out.println(aux);
-            if (aux.length() > 0) {
-                country[i] = parseLine(aux);
-                int help = aux.indexOf(' ');
-                medals[i] = getMedals(aux, n, help+1);
-            }
-        }
-        //System.out.println("Done main, country list");
-        //for (String i : country) {
-        //    System.out.println(i);
-        //}
-        sortOrder(medals, country, n);
-
-        input.close();
+    Country() {
+      this.name = "";
+      this.gold = 0;
+      this.silver = 0;
+      this.copper = 0;
     }
+
+    Country(String name, int gold, int silver, int copper) {
+      this.name = name;
+      this.gold = gold;
+      this.silver = silver;
+      this.copper = copper;
+    }
+
     
-    public static void swapString(String[] c, int src, int dst) {
-        String aux = c[dst];
-        c[dst] = c[src];
-        c[src] = aux;   
+    // i don't really want to deal with get/setter? seems no place in here.
+    // the entire class could've been public.
+
+    public String getName()   { return this.name; }
+    public int getGold()   { return this.gold; }
+    public int getSilver() { return this.silver; }
+    public int getCopper() { return this.copper; }
+
+    public void setName(String name)  { this.name = name; }
+    public void setGold(int gold)     { this.gold = gold; }
+    public void setSilver(int silver) { this.silver = silver; }
+    public void setCopper(int copper) { this.copper = copper; }
+  }
+
+  public static int power(int a, int b) {
+    int num = 1;
+    if (b <= 0) return 1;
+    else {
+      for (int i = 0; i < b; i++)
+        num *= a;
     }
-    
-    public static void swapInt(int[][] c, int src, int dst) {
-        // gold
-        int aux = c[dst][0];
-        c[dst][0] = c[src][0];
-        c[src][0] = aux;
-        
-        // silver
-        aux = c[dst][1];
-        c[dst][1] = c[src][1];
-        c[src][1] = aux;
+    return num;
+  }
 
-        // copper
-        aux = c[dst][2];
-        c[dst][2] = c[src][2];
-        c[src][2] = aux; 
-    }  
-            
+  public static int toInt(String str) {
+    int len = str.charAt(str.length() - 1) == '\n' ? str.length() - 2 : str.length();
+    int num = 0;
+    for (int i = 0; i < len; i++) {
+      int mult = power(10, len-i-1);
+      num += (int) (mult * (char)(str.charAt(i) - '0'));
+    }
+    return num;
+  }
 
-    public static void sortOrder(int[][] medals, String[] country, int n) {
-        // gold
-        for (int i = 0; i < n; i++) {
-            int goldi = medals[i][0]; // ouro
-            int silveri = medals[i][1]; // prata
-            int copperi = medals[i][2]; // bronze
-            String countryi = country[i]; // pais
-            // move left
-            for (int j = 0; j < n; j++) {
-                int goldj = medals[j][0]; // ouro
-                int silverj = medals[j][1]; // prata
-                int copperj = medals[j][2]; // bronze
-                String countryj = country[j]; // pais
+  public static String[] splitOnSpace(String line) {
+    StringBuilder temp = new StringBuilder();
+    ArrayList<String> parts = new ArrayList<String>();
 
-                boolean toSwap = false;
+    for (int i = 0; i < line.length(); i++) {
+      Character c = line.charAt(i);
+      if (c == ' ') {
+        parts.add(temp.toString());
+        temp.setLength(0);
+      } else {
+        temp.append(c);
+      }
+    }
+    parts.add(temp.toString());
+    // System.out.println(parts);
+    return parts.toArray(new String[0]);
+  }
 
-                if (goldi != goldj) { // j -> i
-                    toSwap = goldj > goldi;
-                } else if (silveri != silverj) {
-                    toSwap = silverj > silveri;
-                } else if (copperi == copperj) {
-                    toSwap = copperj > copperi;
-                } else if (copperi == copperj) {
-                    toSwap = countryj.compareTo(countryi) < 0;
-                }
+  public static void swap(Country[] c, int src, int dst) {
+    Country temp = c[dst];
+    c[dst] = c[src];
+    c[src] = temp;
+  }
 
-                if (toSwap) {
-                    swapString(country, j, i);
-                    swapInt(medals, j, i);
-                }
-            }
-        }
+  public static int compareString(String a, String b) {
+    int len = a.length() > b.length() ? b.length() : a.length();
+    int res = 0;
+    int i = 0;
+    while(i < len && res == 0) {
+      res += (int) ((char) b.charAt(i) - a.charAt(i));
+      i++;
+    }
+    return res;
+  }
+  
+  public static boolean compareCountry(Country a, Country b) {
+    if (a.gold != b.gold)
+      return b.gold > a.gold;
+    else if (a.silver != b.silver)
+      return b.silver > a.silver;
+    else if (a.copper != b.copper)
+      return b.copper > a.copper;
+    else
+      return compareString(b.name, a.name) > 0;
+  }
+  
+  public static void sort(Country[] c) {
+    for (int i = 0; i < c.length; i++) {
+      int temp = i;
+      for (int j = i + 1; j < c.length; j++) {
+        if (compareCountry(c[temp], c[j]))
+          temp = j;
+      }
+      if (temp != i)
+        swap(c, i, temp);
+    }      
+  }
+  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = toInt(input.nextLine());
+    Country[] countries = new Country[n];
+    String[] line;
 
-        for (int c = n-1; c >= 0; c--) {
-            System.out.printf("%s %d %d %d\n", country[c], medals[c][0], medals[c][1], medals[c][2]);
-        }
+    for (int i = 0; i < n; i++) {
+      String currLine = input.nextLine();
+      line = splitOnSpace(currLine);
+      countries[i] = new Country(line[0], toInt(line[1]), toInt(line[2]), toInt(line[3]));
     }
 
-
-    public static int toNumber(String num) {
-        int decimalCase;
-        if (num.length() == 1)
-            decimalCase = 1;
-        else
-            decimalCase = 10 * (num.length() - 1);
-        //System.out.println("o: " + num.length() + "\tn: " + decimalCase);
-        int res = -1;
-        for (int i = 0; i < num.length(); i++) {
-            res = (int) (num.charAt(i) - '0') * (decimalCase);
-            decimalCase = decimalCase / 10;
-        }
-        //System.out.println("f: " + res);
-        return res;
+    sort(countries);
+    for (Country c: countries) {
+      System.out.printf("%s %d %d %d\n", c.name, c.gold, c.silver, c.copper);
     }
-
-    public static int[] getMedals(String line, int n, int start) {
-            int[] res = {-1, -1, -1};
-            StringBuilder aux = new StringBuilder();
-            int spaceCount = 0;
-            int medals = 0;
-            //System.out.println(line + ": " + start + ": " + line.charAt(8));
-            for (int i = 0; i < line.length(); i++) {
-                Character c = line.charAt(i);
-                if (c == ' ' && spaceCount == 0) {
-                    spaceCount++;
-                } else if (c == ' ' && spaceCount > 0) {
-                    // TODO: resolve finished string
-                    res[medals] = Integer.parseInt(aux.toString());   // toNumber(aux.toString());
-                    medals++;
-                    aux.setLength(0);
-                } else if (spaceCount > 0 && c != ' ')
-                    aux.append(c);
-            }
-            res[medals] = Integer.parseInt(aux.toString()); //toNumber(aux.toString());
-            return res;
-    }
-
-    public static String parseLine(String line) {
-        StringBuilder res = new StringBuilder();
-        int i = 0;
-        Character c = line.charAt(i);
-        while (c != ' ') {
-            res.append(c);
-            i++;
-            c = line.charAt(i);
-        }
-        return res.toString();
-    }
+    input.close();
+  }
 }
-
