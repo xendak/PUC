a7454ebf858e834c44554ba484a0f74300e0e2a1
Rafael Grossi <rg.grossi@outlook.com>
2025-03-27 17:24:57 -0300

    q1 start

--- AEDS2/provas/3/q1.java
+++ AEDS2/provas/3/q1.java
@@ -1,1 +1,23 @@
 import java.util.Scanner;
+
+public class q1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    int m = input.nextInt();
+    input.nextLine(); // skip line.
+
+    do {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          
+        }
+        input.nextLine();
+      }
+      int n = input.nextInt();
+      int m = input.nextInt();
+      input.nextLine(); // skip line.
+    } while (n > 0 && m > 0);
+    input.close();
+  }
+}
